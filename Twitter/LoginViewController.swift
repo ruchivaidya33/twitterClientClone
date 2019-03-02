6aab25ce18632fe0f0a633ec118c7bad492593a6
Honors <honors@honorslapt-b59853.local>
2019-03-01 19:14:06 -0500

    Twitter Clone Part 1 With Login

---
+++ Twitter/LoginViewController.swift
@@ -0,0 +1,57 @@
+//
+//  LoginViewController.swift
+//  Twitter
+//
+//  Created by Honors on 3/1/19.
+//  Copyright © 2019 Dan. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var loginButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+       // loginButton.layer.borderWidth = 1
+        loginButton.layer.cornerRadius = 15
+        
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func onTapLogin(_ sender: Any) {
+        //find way for user to login
+        let urlString = "https://api.twitter.com/oauth/request_token"
+        TwitterAPICaller.client?.login(url: urlString, success: {
+            //on login success, present hometableVC
+            self.performSegue(withIdentifier: "loginToHome", sender: self)
+            
+        }, failure: { (Error) in
+            //notify user errot has occured
+            print("Error. Could not log in.")
+            
+            let title = "Error"
+            let message = "An error has occured. Unable to log-in."
+            let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+            
+            //set up alert controller
+        })
+        
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
